212016fbfa2647ee2e11138a5b378be6511fd6e1
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-10 18:50:20 +0200

    auto commit

--- app/models/Config.class.php
+++ app/models/Config.class.php
@@ -3,14 +3,14 @@
 class Config {
 	public static function get($path = null) {
 		if ($path) {
-			 $config = $GLOBALS['config'];
-			 $path = explode('/', $path);
-			 foreach ($path as $bit) {
-			 	if (isset($config[$bit])) {
-			 		$config = $config[$bit];
-			 	}
-			 }
-			 return ($config);
+			$config = $GLOBALS['config'];
+			$path = explode('/', $path);
+			foreach ($path as $bit) {
+				if (isset($config[$bit])) {
+					$config = $config[$bit];
+				}
+			}
+			return ($config);
 		}
 		return (false);
 	}
