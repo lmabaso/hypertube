b9cc05e195ae2dbb3519c3d18b01d53d3f00220b
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-10 18:49:35 +0200

    auto commit

---
+++ app/views/video_show.php
@@ -0,0 +1,13 @@
+<div class="main-section">
+    <div style="flex-grow: 20">
+        <!-- <video width="800" controls>
+            <source src="mov_bbb.mp4" type="video/mp4">
+            <source src="mov_bbb.ogg" type="video/ogg">
+            Your browser does not support HTML5 video.
+        </video> -->
+    </div>
+    <div style="flex-grow: 1">
+        videos to watch<br>
+        alskfjaslfjklsd
+    </div>
+</div>
