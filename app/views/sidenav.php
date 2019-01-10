e89c8ed65dca55d0b2c56de485d1145b93a34e05
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-10 18:49:16 +0200

    auto commit

---
+++ app/views/sidenav.php
@@ -0,0 +1,7 @@
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&#9776;</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+</div>
