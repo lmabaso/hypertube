7c109379f1f2fb1471bfc659fdd646776a91ab27
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-14 18:45:22 +0200

    auto commit

--- app/views/header.php
+++ app/views/header.php
@@ -6,6 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HyperTube</title>
     <link href="public/css/main.css" rel="stylesheet">
+    <link href="public/css/reset.css" rel="stylesheet">
+    <link href="public/css/nav.css" rel="stylesheet">
+    <link href="public/css/modal.css" rel="stylesheet">
+    <link href="public/css/cards.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
