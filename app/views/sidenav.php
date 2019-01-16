46e1c6de1830d2c079c425caea9033633f0868c9
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-16 11:10:36 +0200

    auto commit

--- app/views/sidenav.php
+++ app/views/sidenav.php
@@ -1,5 +1,6 @@
 <div id="mySidenav" class="sidenav">
-    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&#9776;</a>
+
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&#9776;HyperTube</a>
     <a href="#">Movies</a>
     <a href="#">Series</a>
 </div>
