abebd90ae5e475a8bf3b032103b904533a6e8818
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-16 10:53:08 +0200

    auto commit

--- app/views/video_show.php
+++ app/views/video_show.php
@@ -1,38 +1,4 @@
 <div class="main-section">
-    <!-- <div style="flex-grow: 20"> -->
-        <!-- <video width="800" controls>
-            <source src="mov_bbb.mp4" type="video/mp4">
-            <source src="mov_bbb.ogg" type="video/ogg">
-            Your browser does not support HTML5 video.
-        </video> -->
-    <!-- <ul id="authors"></ul> -->
-
-    <div class="row">
-        <div class="data-card card">
-            <div class="wrapper">
-            <div class="date">
-                <span class="day">12</span>
-                <span class="month">Aug</span>
-                <span class="year">2016</span>
-            </div>
-            <div class="data">
-                <div class="content">
-                <span class="author">Jane Doe</span>
-                <h1 class="title"><a href="#">Boxing icon has the will for a couple more fights</a></h1>
-                <p class="text">The highly anticipated world championship fight will take place at 10am and is the second major boxing blockbuster in the nation after 43 years.</p>
-                <label for="show-menu" class="menu-button"><span></span></label>
-                </div>
-                <input type="checkbox" id="show-menu" />
-                <ul class="menu-content">
-                <li>
-                    <a href="#" class="fa fa-bookmark-o"></a>
-                </li>
-                <li><a href="#" class="fa fa-heart-o"><span>47</span></a></li>
-                <li><a href="#" class="fa fa-comment-o"><span>8</span></a></li>
-                </ul>
-            </div>
-            </div>
-        </div>
-    </div>
+    <div class="mylib" id="authors"></div>
 </div>
 <script src="public/js/library.js"></script>
