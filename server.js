00b2e074201b0d5c740cfe19c0b27dea28abeddd
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-31 17:23:57 +0200

    auto commit

--- server.js
+++ server.js
@@ -33,7 +33,27 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use("/api/movies", movies);
+// app.use("/api/download", download);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () =>console.log(`Server running on port ${port}`));
+
+// torrent stuff
+
+const torrentStream = require('torrent-stream');
+const fs = require("fs");
+const bencode = require("bencode");
+
+const torrent = bencode.decode(fs.readFileSync("torrents/Beyond.torrent"));
+
+var engine = torrentStream("magnet:?xt=urn:btih:84DC54D20406D0ED8E9339B9FD615D819D7039C0");
+
+engine.on('download', function() {
+  engine.files.forEach(function(file) {
+    console.log("filename:", file.name);
+    var stream = file.createReadStream({ start: 10, end: 100 });
+
+    // stream is readable stream to containing the file content
+  });
+});
