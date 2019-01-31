26578aaf71a9d568bef752ac821d43f4c27b9af6
Liberty MABASO <lmabaso@e4r11p1.wethinkcode.co.za>
2019-01-31 17:23:50 +0200

    auto commit

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const keys = require("../../config/keys");
+const fs = require('fs');
 
 // @route   GET api/movies
 // desc     Get Populor Movies
 // @acces   Public
-router.get("/", (req, res) => res.json({ msg: "Users works" }));
+router.get("/", (req, res) => {
+  fs.readFile('../../movies/file.txt','utf8', (err, data) => {
+    console.log(data);
+  });
+  res.json({ msg: "Users works" })
+});
 
 module.exports = router;
